88cbc0fe1a9a9281a517dbf09e9594cd399c1544
Michaela <m.finger@mail.de>
2019-12-27 16:15:28 +0100

    add sound to margins

---
+++ Sonified Tumor Resection/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    void Start()
+    {
+        // todo check where the tool is and start the correct sound (would probably always be outside the cutting area in the "too far" section)
+    }
+
+  public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+
+  public void Stop (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Stop();
+    }
+
+    public void StopAll()
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            sounds[i].source.Stop();
+        }
+    }
+}
---
+++ Sonified Tumor Resection/Assets/Scripts/ToolCollider.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolCollider : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        FindObjectOfType<AudioManager>().StopAll();
+        // start playing sound depending on the area that was entered
+        if (col.gameObject.CompareTag("OuterErrorMargin"))
+        {
+            FindObjectOfType<AudioManager>().Play("OuterErrorMargin");
+        }
+        else if (col.gameObject.CompareTag("ResectionArea"))
+        {
+            FindObjectOfType<AudioManager>().Play("ResectionArea");
+        }
+        else if (col.gameObject.CompareTag("InnerErrorMargin"))
+        {
+            FindObjectOfType<AudioManager>().Play("InnerErrorMargin");
+        }
+        else if (col.gameObject.CompareTag("TumorArea"))
+        {
+            FindObjectOfType<AudioManager>().Play("TumorArea");
+        }
+
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+
+        // stop sound depending on the area that was exited
+        if (col.gameObject.CompareTag("OuterErrorMargin"))
+        {
+            FindObjectOfType<AudioManager>().Stop("OuterErrorMargin");
+            FindObjectOfType<AudioManager>().Play("NoArea");
+        }
+        else if (col.gameObject.CompareTag("ResectionArea"))
+        {
+            FindObjectOfType<AudioManager>().Stop("ResectionArea");
+            FindObjectOfType<AudioManager>().Play("OuterErrorMargin");
+        }
+        else if (col.gameObject.CompareTag("InnerErrorMargin"))
+        {
+            FindObjectOfType<AudioManager>().Stop("InnerErrorMargin");
+            FindObjectOfType<AudioManager>().Play("ResectionArea");
+        }
+        else if (col.gameObject.CompareTag("TumorArea"))
+        {
+            FindObjectOfType<AudioManager>().Stop("TumorArea");
+            FindObjectOfType<AudioManager>().Play("InnerErrorMargin");
+        }
+
+    }
+}
