8ee160a294b91b238c918a682edc2afdb4d4dc85
SvenjaSL <59289932+SvenjaSL@users.noreply.github.com>
2019-12-29 17:13:04 +0100

    Revert "Revert "ADDED: Functionality of looping a sound""
    This reverts commit 0b8aba8ca032427d7dbcaff06745eb715ae53898.

---
+++ Sonified Tumor Resection/Assets/Scripts/RotateObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public KeyCode pressUp;
+    public KeyCode pressDown;
+    public KeyCode pressLeft;
+    public KeyCode pressRight;
+    public KeyCode pressForward;
+    public KeyCode pressBackward;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(pressLeft))
+        {
+            transform.Rotate(0.0f, -10, 0.0f);
+        }
+        if (Input.GetKeyDown(pressRight))
+        {
+            transform.Rotate(0.0f, +10, 0.0f);
+        }
+        if (Input.GetKeyDown(pressForward))
+        {
+            transform.Rotate(+10, 0.0f, 0.0f);
+        }
+        if (Input.GetKeyDown(pressBackward))
+        {
+            transform.Rotate(-10, 0.0f, 0.0f);
+        }
+        if (Input.GetKeyDown(pressUp))
+        {
+            transform.Rotate(0.0f, 0.0f, +5);
+        }
+        if (Input.GetKeyDown(pressDown))
+        {
+            transform.Rotate(0.0f, 0.0f, -5);
+        }
+    }
+}
