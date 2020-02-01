3bd3498ac456d9700966e553d863b6d8f3039f13
Michaela <m.finger@mail.de>
2020-02-02 00:50:43 +0100

    fix size of tumor and margins

--- Sonified Tumor Resection/Assets/Scripts/Constants.cs
+++ Sonified Tumor Resection/Assets/Scripts/Constants.cs
@@ -6,8 +6,8 @@ public static class Constants
     public const bool DebugLogAll = false;
 
     // -----------distances & sizes-----------------
-    public const float CuttingAreaSize = 0.02f;
-    public const float ErrorMarginSize = 0.01f;
+    public const float CuttingAreaSize = 0.015f;
+    public const float ErrorMarginSize = 0.005f;
     public const float TotalMaxDistance = CuttingAreaSize + 2 * ErrorMarginSize; //total max distance from the tumor
     public const float MaxObstacleDistance = 0.01f; //lower than the max distance to blood vessel triggers a sonification
 
